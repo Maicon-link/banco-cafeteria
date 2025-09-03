37de74ef130b561481c553419f6a374fd81b56e9
Maicon <maiconlink99@gmail.com>
2025-09-02 23:55:49 -0300

    Alterei a tabela de pedidos e criei mais uma tabela de 'ItensPedido'

--- banco.sql
+++ banco.sql
@@ -7,35 +7,37 @@ CREATE TABLE Produtos (
 
 CREATE TABLE Pedidos (
     id INTEGER PRIMARY KEY,
-    produto_id INTEGER NOT NULL,
-    quantidade INTEGER NOT NULL,
-    data_pedido DATE,
-    FOREIGN KEY (produto_id) REFERENCES Produtos(id)
+    data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 
-INSERT INTO Produtos (nome, preco) VALUES
-('Café Expresso', 8.90),
-('Cappuccino', 13.90),
-('Pão de Queijo', 5.90),
-('Café com leite', 10.90),
-('Omelete', 8.00),
-('Chocolate quente', 12.90),
-('Misto quente', 13.90),
-('Queijo quente', 14.90),
-('Pão de queijo recheado', 9.00),
-('Suco de laranja', 9.00);
+INSERT INTO Pedidos DEFAULT VALUES; -- pedido_id = 1
+INSERT INTO Pedidos DEFAULT VALUES; -- pedido_id = 2
 
 
 
-INSERT INTO Pedidos (produto_id, quantidade, data_pedido) VALUES
-(1, 2, '25/09/2025'), -- 2 cafés expressos
-(2, 1, '25/09/2025'), -- 1 cappuccino
-(3, 3, '25/09/2025'), -- 3 pães de queijo
-(4, 2, '25/09/2025'), -- 2 cafés com leite
-(5, 3, '25/09/2025'), -- 3 omeletes
-(6, 1, '25/09/2025'), -- 1 chocolate quente
-(7, 2, '25/09/2025'), -- 2 misto quente
-(8, 4, '25/09/2025'), -- 4 queijo quente
-(9, 3, '25/09/2025'), -- 3 pães de quejio recheados
-(10, 1, '25/09/2025'); -- 1 suco de laranja
+CREATE TABLE ItensPedido (
+    id INTEGER PRIMARY KEY,
+    pedido_id INTEGER NOT NULL,
+    produto_id INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL,
+    FOREIGN KEY (pedido_id) REFERENCES Pedidos(id),
+    FOREIGN KEY (produto_id) REFERENCES Produtos(id)
+);
+
+-- Pedido 1
+INSERT INTO ItensPedido (pedido_id, produto_id, quantidade) VALUES
+(1, 1, 2),  -- 2 Café Expresso
+(1, 2, 1),  -- 1 Cappuccino
+(1, 3, 3),  -- 3 Pães de Queijo
+(1, 4, 2),  -- 2 Café com leite
+(1, 5, 3);  -- 3 Omeletes
+
+-- Pedido 2
+INSERT INTO ItensPedido (pedido_id, produto_id, quantidade) VALUES
+(2, 6, 1),  -- 1 Chocolate quente
+(2, 7, 2),  -- 2 Misto quente
+(2, 8, 4),  -- 4 Queijo quente
+(2, 9, 3),  -- 3 Pão de queijo recheado
+(2, 10, 1); -- 1 Suco de laranja
+
